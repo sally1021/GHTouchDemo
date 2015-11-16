cc39970bc2a3c48d72836f293cd3f244ed575b30
吴贵卉 <wugh@n2n2n.com>
2015-11-16 16:13:26 +0800

    Initial Commit

---
+++ GHTouchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHTouchDemo
+//
+//  Created by sally on 15/11/16.
+//  Copyright © 2015年 koalac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GHTouchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHTouchDemo
+//
+//  Created by sally on 15/11/16.
+//  Copyright © 2015年 koalac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
